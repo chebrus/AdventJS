d9d90f978fbdeb610b7103bb6e707947ba54fb95
CHE <mo.monteroapaza@gmail.com>
2022-09-29 20:08:14 -0500

    add challenges 07

---
+++ 07.js
@@ -0,0 +1,33 @@
+const almacen = {
+    'estanteria1': {
+        'cajon1': {
+            'producto1': 'coca-cola',
+            'producto2': 'fanta',
+            'producto3': 'sprite'
+        }
+    },
+    'estanteria2': {
+        'cajon1': 'vacio',
+        'cajon2': {
+            'producto1': 'pantalones',
+            'producto2': 'camiseta' // <- ¡Está aquí!
+        }
+    }
+}
+
+const contains = (store, product) => {
+    return  JSON.stringify(store).includes(`:"${product}"`) // No aceptado?
+}
+console.log(contains(almacen, 'Camiseta'))
+
+const otroAlmacen = {
+    'baul': {
+        'fondo': {
+            'objeto': 'cd-rom',
+            'otro-objeto': 'disquette',
+            'otra-cosa': 'mando'
+        }
+    }
+}
+
+console.log(contains(otroAlmacen, 'gameboy'))
